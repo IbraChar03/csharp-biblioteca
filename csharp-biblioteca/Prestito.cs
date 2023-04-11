f68027d7b0f4602e604aebcdb2a6913c131e60e8
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-04-11 15:28:55 +0200

    classe biblioteca e prestito

---
+++ csharp-biblioteca/Prestito.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Prestito
+    {
+        public Utente Utente { get; set; }
+        public string PeriodoInizio { get; set; }
+        public string PeriodoFine { get; set; }
+        public Documento Documento { get; set; }
+
+        public Prestito(Utente utente, string periodoInizio, string periodoFine, Documento documento)
+        {
+            Utente = utente;
+            PeriodoInizio = periodoInizio;
+            PeriodoFine = periodoFine;
+            Documento = documento;
+        }
+    }
+}
