34882b33abebff210f9e8fc8a22460e0997d9756
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-04-11 15:22:58 +0200

    classe dvd

---
+++ csharp-biblioteca/Dvd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Dvd : Documento
+    {
+        public int Durata { get; set; }
+        public Dvd(int durata, string codice, string titolo, int anno, string settore, string autore, int scaffale) : base(codice, titolo, anno, settore, autore, scaffale)
+        {
+            Durata = durata;
+        }
+    }
+}
