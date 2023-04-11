48fcf3782ddd964bf5d599b0cef70f38858eda93
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-04-11 17:32:45 +0200

    commenti

--- csharp-biblioteca/Biblioteca.cs
+++ csharp-biblioteca/Biblioteca.cs
@@ -13,12 +13,7 @@ namespace csharp_biblioteca
         public List<Documento> Documenti = new List<Documento>();
         public Utente Utente { get; set; }
   
-        //public Biblioteca(List<Utente> utenti, List<Prestito> prestiti, List<Documento> documenti)
-        //{
-        //    Utenti = utenti;
-        //    Prestiti = prestiti;
-        //    Documenti = documenti;
-        //}
+       
         public void creaUtente(string cognome,string nome,string email,string password, int telefono)
         {
             
@@ -29,39 +24,7 @@ namespace csharp_biblioteca
         }
         public void RicercaLibro()
         {
-            //Utente utente1 = new Utente("Rossi", "Mario", "mario@gmail.com", "ciao123", 34344233);
-            //Libro libro1 = new Libro(232, "ABCD45", "Titolo1", 2003, "Fantasy", "Luigi Verdi", 4);
-            //Libro libro2 = new Libro(123, "GFGE32", "Titolo2", 2006, "Horror", "Mario Rossi", 7);
-            //Libro libro3 = new Libro(443, "DGFS12", "Titolo3", 2013, "Fantasy", "Paolo Gialli ", 2);
-            //Prestito prestito1 = new Prestito(utente1, "11/04/2023", "11/05/2023", libro1);
-            //Utenti.Add(utente1);
-            //Documenti.Add(libro1);
-            //Documenti.Add(libro2);
-            //Documenti.Add(libro3);
-            //Prestiti.Add(prestito1);
-            //Console.WriteLine("Benvenuto nella biblioteca!");
-            //Console.WriteLine("Registrati per iniziare");
-
-            //Console.Write("Nome : ");
-            //string nomeUtente = Console.ReadLine();
-
-            //Console.Write("Cognome : ");
-            //string cognomeUtente = Console.ReadLine();
-
-            //Console.Write("Indirizzo Email : ");
-            //string email = Console.ReadLine();
            
-
-            //Console.Write("Password : ");
-            //string password = Console.ReadLine();
-
-            //Console.Write($"Numero di telefono : ");
-            //int telefono;
-            //while (!int.TryParse(Console.ReadLine(), out telefono))
-            //    Console.WriteLine("Inserisci dei numeri");
-            //Utente nuovoUtente = new Utente(cognomeUtente, nomeUtente, email, password, telefono);
-            //Utenti.Add(nuovoUtente);
-            //Console.WriteLine("Ti sei registrato correttamente!");
             Console.Write("Scrivi il titolo di un libro : ");
             string titolo = Console.ReadLine();
             var ricercaLibro = Documenti.Find(x => x.Titolo == titolo);
