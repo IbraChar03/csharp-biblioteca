545f4c448f2085e19c980221595a7acf840b8901
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-04-11 15:10:39 +0200

    classe utente

--- csharp-biblioteca/Utente.cs
+++ csharp-biblioteca/Utente.cs
@@ -8,6 +8,19 @@ namespace csharp_biblioteca
 {
     internal class Utente
     {
-        
+        public string cognome { get; set; }
+        public string nome { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public int telefono { get; set; }
+
+        public Utente(string cognome, string nome, string email, string password, int telefono)
+        {
+            this.cognome = cognome;
+            this.nome = nome;
+            this.email = email;
+            this.password = password;
+            this.telefono = telefono;
+        }
     }
 }
